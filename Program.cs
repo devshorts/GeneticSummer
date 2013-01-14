892bbbb59ac6be118dc312a73ec3aa5816e8dda6
anton <akropp@gmail.com>
2013-01-14 12:02:08 -0500

    adding

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Linq;
+using Evolve.Common;
+using Evolve.Framework;
+using Evolve.Individuals;
 
 namespace Evolve
 {
@@ -8,31 +11,35 @@ namespace Evolve
         static void Main(string[] args)
         {
             var config = new EvolveConfig
+            {
+                PopulationConfig = new PopulationConfig
                 {
-                    PopulationConfig = new PopulationConfig
-                        {
-                            IndividualSize = 100,
-                            Max = 20,
-                            Min = 0,
-                            MutationChance = 1,
-                            MutationAmount = 2,
-                            PopulationSize = 100,
-                            Target = 10.9
-                        },
-                    Precision = 1
-                };
+                    MutationChance = 1,
+                    PopulationSize = 100,
+                    IndividualConfig = new IndividualConfig
+                    {
+                        MutationAmount = 2,
+                        IndividualSize = 5,
+                        Max = 20,
+                        Min = 0,
+                        Target = 10.9
+                    }
+                },
+                Precision = 1
+            };
+
             var evolver = new Evolver
-                {
-                    Config = config
-                };
+            {
+                Config = config
+            };
 
             var finalPopulation = evolver.Evolve();
 
             Console.WriteLine("Final population fitness {0}", finalPopulation.Grade());
             Console.WriteLine("Accepted values:");
-            finalPopulation.Individuals.Where(i=> Math.Round(i.Sum, config.Precision) == Math.Round(config.PopulationConfig.Target, config.Precision))
+            finalPopulation.Individuals.Where(i=> Math.Round((i as Individual).Sum, config.Precision) == Math.Round(config.PopulationConfig.IndividualConfig.Target, config.Precision))
                                        .GroupBy(i => i.ToString())
-                                       .ForEach(i=> Console.WriteLine("{0} indiviuals {1} sum to {2}", i.Count(), i.First(), i.First().Sum));
+                                       .ForEach(i=> Console.WriteLine("{0} indiviuals {1} sum to {2}", i.Count(), i.First(), (i.First() as Individual).Sum));
         }
     }
 }
